03e6510da05f4d1d9d1f365d0e131ef3e400238e
w0478181 <w0478181@campus.nscc.com>
2024-02-01 11:12:59 -0400

    PROG1400

--- Inheritance/Super_Class_OOP3D.py
+++ Inheritance/Super_Class_OOP3D.py
@@ -12,8 +12,12 @@ class Rectangle:
     def calculate_volume(self):
         return self.length * self.width * self.height
 
-    
+class Cube(Rectangle):
+    def __init__(self, side_length):
+        super().__init__(length = side_length, width = side_length, height = side_length)
+
 my_rectangle  = Rectangle(length=5, width=3, height=6)
+my_cube = Cube(side_length = 5)
 
 print(f"Length: {my_rectangle.length}")
 print(f"Width: {my_rectangle.width}")
@@ -24,4 +28,11 @@ volume = my_rectangle.calculate_volume()
 print("Surface Area:",surface_area)
 print("Volume:",volume)
 
+print("Square Side Length:",my_cube.length)
+print("Square Side Width:",my_cube.width)
+print("Square Side Heigth:",my_cube.height)
 
+surface_area_cube = my_cube.calculate_surface_area()
+volume_cube = my_cube.calculate_volume()
+print("Perimeter of Square:",volume_cube)
+print("Area of Square:",surface_area_cube)
